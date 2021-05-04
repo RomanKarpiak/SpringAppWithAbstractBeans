c9ef9a118e85cd1d53533ed3bf2f35dc133f567a
Roman Karpiak <roman.karpiak@gmail.com>
2021-05-05 01:27:34 +0400

    Task3 is done

--- src/main/java/TestApp.java
+++ src/main/java/TestApp.java
@@ -1,7 +1,4 @@
-import com.roman.domain.A;
-import com.roman.domain.B;
-import com.roman.domain.C;
-import com.roman.domain.E;
+import com.roman.domain.*;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestApp {
@@ -12,10 +9,13 @@ public class TestApp {
         C c = context.getBean("C", C.class);
         C c1 = context.getBean("C", C.class);
         E e = context.getBean("E", E.class);
+        F f = context.getBean("F", F.class);
         System.out.println(a);
         System.out.println(b);
         System.out.println(c.getD());
         System.out.println(c1.getD());
         e.printClassName();
+        f.greeting();
+        context.close();
     }
 }
---
+++ src/main/java/com/roman/domain/F.java
@@ -0,0 +1,56 @@
+package com.roman.domain;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class F implements BeanNameAware,
+        ApplicationContextAware,
+        InitializingBean,
+        DisposableBean {
+    @Override
+    public void setBeanName(String s) {
+        System.out.println("---setBeanName executed---");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("--- setApplicationContext executed ---");
+    }
+    @PostConstruct
+    public void postConstruct(){
+        System.out.println("--- @PostConstruct executed ---");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("--- afterPropertiesSet executed ---" +
+                "Bean F has been instantiated.");
+    }
+    public void doMyInit() {
+        System.out.println("--- init-method ---" +
+                "Doing initialization class F");
+    }
+    public void greeting() {
+        System.out.println("Hello from class F!");
+    }
+    @PreDestroy
+    public void preDestroy(){
+        System.out.println("--- @PreDestroy executed ---");
+    }
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("--- destroy executed --- Container has been closed.");
+    }
+
+    public void doMyDestroy() {
+        System.out.println("--- destroy-method executed --- Doing my destruction...");
+    }
+
+}
